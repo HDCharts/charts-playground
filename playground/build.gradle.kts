3bef8413437dff7db4f2b63b6576b651fe234aa1
HDCode <hdcodedev@gmail.com>
2026-03-15 23:44:38 +0100

    Enable Kotlin serialization plugin for playground

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.serialization)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.build.config)
     alias(libs.plugins.compose.compiler)
